ba1f274e680753d031570dcd6d05cba5fab81dcd
Chelsea Rickman <chelsearickman@Chelseas-MacBook-Air.local>
2019-09-12 14:52:15 -0700

    Entered Questions

---
+++ projects/curly-girl/scripts/quiz-content.js
@@ -0,0 +1,94 @@
+window.quizContent = [
+    {
+        question: "It's wash day!<br />How are you washing your hair today?",
+        choices: [
+            {
+                choice: "Cowash",
+                isCorrect: true,
+                incorrectMessage: "",
+            },
+            {
+                choice: "Lo-poo",
+                isCorrect: false,
+                incorrectMessage: "Oops! The lo-poo was too drying",
+            },
+        ],
+    },
+    {
+        question: "Time for conditioner. Don't forget to STC!<br />\
+        How much conditioner are you using?",
+        choices: [
+            {
+                choice: "Maybe a quarter-sized amount",
+                isCorrect: false,
+                incorrectMessage: "There is no way that is enough!",
+            },
+            {
+                choice: "The whole dang bottle",
+                isCorrect: true,
+                incorrectMessage: "",
+            },
+        ],
+    },
+    {
+        question: "Leave-in. Which one are you going to use?",
+        choices: [
+            {
+                choice: "Leave-in conditioner with protein",
+                isCorrect: true,
+                incorrectMessage: "",
+            },
+            {
+                choice: "Leave-in conditioner without protein",
+                isCorrect: false,
+                incorrectMessage: "Ooh. Your hair is too soft and mushy-feeling now. You need some protein!",
+            },
+        ]
+    },
+    {
+        question: "Let's lock those curls in place!",
+        choices: [
+            {
+                choice: "Gel",
+                isCorrect: true,
+                incorrectMessage: "",
+            },
+            {
+                choice: "Mousse",
+                isCorrect: false,
+                incorrectMessage: "Not enough hold. Your curls lost their form!",
+            },
+        ]
+    },
+    {
+        question: "Time to dry. What are you doing?",
+        choices: [
+            {
+                choice: "Diffusing",
+                isCorrect: true,
+                incorrectMessage: "",
+            },
+            {
+                choice: "Air drying",
+                isCorrect: false,
+                incorrectMessage: "Ain't nobody got time for that!<br />\
+                That will LITERALLY take 4 days, 17 hours, and 36 minutes",
+            },
+        ]
+    },
+    {
+        question: "SOTC",
+        choices: [
+            {
+                choice: "Bare hands, no product",
+                isCorrect: false,
+                incorrectMessage: "Whoa! Frizz city!",
+            },
+            {
+                choice: "Hands with lube or oil",
+                isCorrect: true,
+                incorrectMessage: "",
+            },
+        ]
+    }
+]
